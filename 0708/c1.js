d25a6fa21aaa49100e5ee459f5dec550c7cf85fe
John Liu <johnliutw@hotmail.com>
2024-07-08 09:52:02 +0800

    practice2

--- 0708/c1.js
+++ 0708/c1.js
@@ -1,3 +1,19 @@
+// practice 2
+let grade = 35;
+if(grade >= 90){
+  console.log('A');
+} else if (grade >= 80){
+  console.log('B');
+} else if (grade >= 70){
+  console.log('C');
+} else if (grade >= 60){
+  console.log('D');
+} else {
+  console.log('E');
+}
+
+
+// practice 1
 let chi = 100;
 let math = 100;
 let eng = 80;
