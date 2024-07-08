d7aedcef9bfad0dee918e79c7ef93c94e6393e03
John Liu <johnliutw@hotmail.com>
2024-07-08 10:28:24 +0800

    practice4

--- 0708/c1.js
+++ 0708/c1.js
@@ -1,3 +1,31 @@
+// practice 4
+let weekNumber = 1;
+switch(weekNumber){
+  case 1:
+    console.log('星期一');
+    break;
+  case 2:
+    console.log('星期二');
+    break;
+  case 3:
+    console.log('星期三');
+    break;
+  case 4:
+    console.log('星期四');
+    break;
+  case 5:
+    console.log('星期五');
+    break;
+  case 6:
+    console.log('星期六');
+    break;
+  case 7:
+    console.log('星期日');
+    break;
+  default:
+    console.log('');
+}
+
 // practice 3
 let questions = 50;
 let totalScore = 0;
